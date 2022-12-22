453ea8fd949b104137e4b9058ec98eeaff71e391
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2022-12-22 15:34:02 +0100

    section preload

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+/*****************************Preload*****************************/
+
+//loading will be end after documents is loaded
+
+const preloader = document.querySelector("[data-preaload]");
+
+
+console.log(preloader)
+
+window.addEventListener('load', function () {
+
+    document.body.classList.add('loaded');
+    preloader.classList.add('loaded');
+
+})
