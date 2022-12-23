30b2395b88660426b4f19866e9ed26ac1bda27ae
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2022-12-23 18:41:35 +0100

    navbar mobile complite

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,6 @@
 const preloader = document.querySelector("[data-preaload]");
 
 
-console.log(preloader)
 
 window.addEventListener('load', function () {
 
@@ -16,3 +15,32 @@ window.addEventListener('load', function () {
     preloader.classList.add('loaded');
 
 })
+
+//Add events listener of multiple elements
+
+
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+
+//Navbar 
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+
+
+const togglerNavbar = function () {
+    navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.body.classList.toggle('nav-active');
+
+}
+
+
+addEventOnElements(navTogglers, 'click', togglerNavbar);
