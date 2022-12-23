a7900db6d3c6f0371f20835b8fcdf3a5391bfdf4
Krzysztof Siek <Krzysztof.Siek93@gmail.com>
2022-12-23 20:35:54 +0100

    section navbar almost finish

--- src/js/main.js
+++ src/js/main.js
@@ -44,3 +44,38 @@ const togglerNavbar = function () {
 
 
 addEventOnElements(navTogglers, 'click', togglerNavbar);
+
+
+//Header 
+
+
+/**
+ * HEADER & BACK TOP BTN
+ */
+
+const header = document.querySelector("[data-header]");
+//const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+    const isScrollBottom = lastScrollPos < window.scrollY;
+    if (isScrollBottom) {
+        header.classList.add("hide");
+    } else {
+        header.classList.remove("hide");
+    }
+
+    lastScrollPos = window.scrollY;
+}
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 50) {
+        header.classList.add("active");
+        // backTopBtn.classList.add("active");
+        hideHeader();
+    } else {
+        header.classList.remove("active");
+        // backTopBtn.classList.remove("active");
+    }
+});
